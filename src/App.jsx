fd1b88670411d1a872f5e6252550dcd7cc6aa377
Brian Kim <briankim0206@gmail.com>
2024-03-07 21:55:26 +0900

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import Nav from "./components/Nav";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  HashRouter,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Models from "./pages/Models";
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Nav></Nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path = "/models" element = {<Models/>} />
+        <Route path="/models" element={<Models />} />
       </Routes>
       <Footer></Footer>
-    </Router>
+    </HashRouter>
   );
 }
 
